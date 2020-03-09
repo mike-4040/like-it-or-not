5968f8bb2a04ca52e58b77f80845a37bae0d00bb
Mike Kravtsov <mike4040@me.com>
2020-03-09 16:11:49 -0700

    user routes

--- controllers/user.js
+++ controllers/user.js
@@ -2,16 +2,75 @@
  * User controller.
  */
 const db = require('../models');
+const dbErrors = require('../utils/dbErrors');
 
 module.exports = {
   /**
-   * Find all Users, sort them by lastName, return
+   * Find all User, sort them ascending
    * @param {object} req
    * @param {object} res
+   * @returns {Object[]}
    */
   findAll: (req, res) => {
     db.User.find({})
       .sort({ lastName: 1 })
-      .then(user => res.json(user));
+      .then(users => res.json(users))
+      .catch(err => dbErrors(err, res));
+  },
+  /**
+   * Find one User
+   * @returns {Object}
+   */
+  findOne: (req, res) => {
+    db.User.findById(req.params.id)
+      .then(user => res.json(user))
+      .catch(err => dbErrors(err, res));
+  },
+  /**
+   * Create one User
+   * @returns {Object} category from db.
+   */
+  create: (req, res) => {
+    db.User.create(req.body)
+      .then(dbUser => res.json(dbUser))
+      .catch(err => dbErrors(err, res));
+  },
+  /**
+   * Update a User
+   * @returns {Object} updated category from db.
+   */
+  update: (req, res) => {
+    db.User.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: req.body },
+      { new: true, useFindAndModify: false }
+    )
+      .then(dbUser => res.json(dbUser))
+      .catch(err => dbErrors(err, res));
+  },
+  /**
+   * Delete a User
+   * @returns {Object} category from db.
+   */
+  delete: (req, res) => {
+    db.User.findByIdAndDelete(req.params.id)
+      .then(dbUser => res.json(dbUser))
+      .catch(err => dbErrors(err, res));
+  },
+  auth: (email, password, cb) => {
+    db.User.findOne({email})
+    .then( user => {
+      if (bcrypt.compareSync(password, user.password)) {
+        const token = jwt.sign(
+          { id: user.id, email, fisrtName: user.firstName },
+          process.env.SERVER_SECRET,
+          { expiresIn: 129600 }
+        );
+        cb(token);
+      } else {
+        cb('Wrong password');
+      }
+    })
+    .catch(err => dbErrors(err, res));
   }
 };
--- models/User.js
+++ models/User.js
@@ -13,7 +13,8 @@ const userSchema = new Schema({
   },
   email: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   recordId: [{
     type: Types.ObjectId,

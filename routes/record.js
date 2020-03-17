a6830b8e010f188670997863a882ebfa8a99d785
Mike Kravtsov <mike4040@me.com>
2020-03-16 18:56:32 -0700

    typo fix

--- routes/record.js
+++ routes/record.js
@@ -5,6 +5,7 @@ router.post('/', controller.create);
 router.get('/:id', controller.findOne);
 router.put('/', controller.update);
 router.delete('/:id', controller.delete);
+
 router.get('/:id/record', controller.findRecordsUser);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,5 @@ router.get('/', controller.findAll);
 router.get('/:id', controller.findOne);
 router.delete('/:id', controller.delete);
 router.put('/:id', controller.update);
-router.get('/:id/records', controller.userRecords);
 
 module.exports = router;

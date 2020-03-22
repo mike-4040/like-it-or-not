2e2cb7342a7bfa69c27a47780c53932b8f794620
Mike Kravtsov <mike4040@me.com>
2020-03-21 17:11:47 -0700

    all routes but signin/signup are protected

--- routes/record.js
+++ routes/record.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
 const controller = require('../controllers/record');
-const { auth } = require('../utils/auth');
 
-router.post('/', auth, controller.create);
-router.get('/:id', auth, controller.findOne);
-router.put('/', auth, controller.update);
-router.delete('/:id', auth, controller.delete);
+router.post('/', controller.create);
+router.get('/:id', controller.findOne);
+router.put('/', controller.update);
+router.delete('/:id', controller.delete);
 
 router.get('/:id/record', controller.findRecordsUser);
 

07ec877451065f7fd347db26fe29701b853485d1
konstagap <konstagapov123@gmail.com>
2020-04-06 21:26:06 -0700

    adding picture and little feature to password

--- client/src/components/common/Tabs/TableForm.js
+++ client/src/components/common/Tabs/TableForm.js
@@ -3,21 +3,27 @@ import { Grid, Button, makeStyles } from '@material-ui/core';
 import FormikFieldInput from '../inputElements/FormikFieldInput';
 import UpdateMessage from './UpdateMessage';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   button: {
-    margin: '16px 0',
-  },
+    margin: '16px 0'
+  }
 }));
 
 export default function TableForm({ errors, touched, data }) {
   const classes = useStyles();
+  const [showPassword, setShowPassword] = React.useState(false);
 
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
   return (
     <Grid container justify='space-around'>
       <Grid item xs={12} md={5}>
-        {data.fields.map((el) => {
+        {data.fields.map(el => {
           return (
             <FormikFieldInput
+              showPassword={showPassword}
+              handleClickShowPassword={handleClickShowPassword}
               key={el.name}
               errors={errors}
               touched={touched}
--- client/src/components/common/UserCard.js
+++ client/src/components/common/UserCard.js
@@ -2,42 +2,27 @@ import React from 'react';
 import { Grid, makeStyles, Paper, Avatar, Typography } from '@material-ui/core';
 import PersonIcon from '@material-ui/icons/Person';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    marginTop: '20px',
+    marginTop: '20px'
   },
   paper: {
     margin: '0 20px',
-    padding: '10px',
-    minHeight: theme.spacing(15),
-    minWidth: theme.spacing(40),
-  },
-  large: {
-    width: theme.spacing(7),
-    height: theme.spacing(7),
-  },
-  card: {
-    height: '100%',
+    padding: '20px'
   },
   picture: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingRight: '10px',
-    borderRight: `1px solid ${theme.palette.divider}`,
+    width: theme.spacing(8),
+    height: theme.spacing(8),
+    margin: 'auto'
+  },
+  card: {
+    height: '100%'
   },
   content: {
     display: 'flex',
     flexDirection: 'column',
-    paddingLeft: '20px',
-    justifyContent: 'center',
-  },
-  row: {
-    transition: 'all .4s linear',
-    '&:hover': {
-      transform: 'scale(1.1)',
-    },
-  },
+    justifyContent: 'center'
+  }
 }));
 
 export default function UserCard({ user }) {
@@ -45,41 +30,22 @@ export default function UserCard({ user }) {
 
   return (
     <div className={classes.root}>
-      <Paper variant='outlined' square className={classes.paper}>
-        <Grid container className={classes.card}>
-          <Grid item xs={2} className={classes.picture}>
-            <Avatar className={(classes.large, classes.row)}>
+      <Paper elevation={3} square className={classes.paper}>
+        <Grid container direction='row-reverse' className={classes.card}>
+          <Grid item xs={4}>
+            <Avatar className={classes.picture} src={user?.photo}>
               <PersonIcon />
             </Avatar>
           </Grid>
-          <Grid item xs={10} className={classes.content}>
-            <Typography
-              className={classes.row}
-              component='p'
-              variant='subtitle1'
-            >
+          <Grid item xs={8} className={classes.content}>
+            <Typography component='p' variant='subtitle1'>
               First name : {user.firstName}
             </Typography>
-            <Typography
-              className={classes.row}
-              component='p'
-              variant='subtitle1'
-            >
+            <Typography component='p' variant='subtitle1'>
               Last name : {user.lastName}
             </Typography>
-            <Typography
-              className={classes.row}
-              component='p'
-              variant='subtitle1'
-            >
-              Email : {user.email}
-            </Typography>
-            <Typography
-              className={classes.row}
-              component='p'
-              variant='subtitle1'
-            >
-              Phone : {user?.phone || 'none'}
+            <Typography component='p' variant='subtitle1'>
+              {user.email}
             </Typography>
           </Grid>
         </Grid>

bffa77d1075f1c61fecf03eee671ed849cc81221
Mike Kravtsov <mike4040@me.com>
2020-04-08 15:34:05 -0700

    docs

--- utils/applyPassStratGoogle.js
+++ utils/applyPassStratGoogle.js
@@ -1,9 +1,17 @@
+/**
+ * @module
+ * */
 const passportGoogle = require('passport-google-oauth');
 const { verifyCbGoogle } = require('./auth');
 const { passportrc } = require('../config/config');
 
 const Strategy = passportGoogle.OAuth2Strategy;
-
+/**
+ * @function applyPassStratGoogle set auth straregy
+ *
+ * @param {object} passport passport.js
+ * @returns {null}
+ */
 module.exports = passport => {
   const options = {
     clientID: process.env.GOOGLE_CLIENT_ID,
